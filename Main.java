77716f860419f49f175568abff5fc72745c20caa
keerthi1997 <keerthana.vjps@gmail.com>
2017-07-07 09:43:52 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class Main{
+public static void main(String[] args)throws IOException
+{
+int i;
+String g="Hello";
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+for(i=0;i<5;i++)
+System.out.println(g);
+}
+}
